0b24c8da035d8f83f334db214e85ca87ee083802
nbhaargavi <123439300+nbhaargavi@users.noreply.github.com>
2023-01-27 18:20:09 +0530

    Update Point.java

--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -1,50 +1,29 @@
 package org.example;
 import java.util.Scanner;
-public class credit implements Cloneable {
-    private String cname;
-    private Integer cno;
-    private Integer cnum = 501005463;
-    private Integer expdate;
-
-    public credit( String cname,Integer cno, Integer expdate) {
-        this.cname=cname;
-        this.cno=cno;
-        this.expdate=expdate;
+public class Point implements Cloneable {
+    private Integer x;
+    private Integer y;
+    public Point( Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
     }
     void eq()
     {
-        if(cnum.equals(cno)) {
-            System.out.println("Credit card number matched!");
-        }
-        else
-        {
-            System.out.println("OOPS! Credit card number doesn't match");
-        }
+        System.out.println(x.equals(y));
     }
-    credit cr()
+    protected Object Clone() throws  CloneNotSupportedException
     {
-        try
-        {
-            return (credit) super.clone();
-        }
-        catch(CloneNotSupportedException e)
-        {
-            System.out.println("cloned error");
-            return this;
-        }
+        return super.clone();
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws CloneNotSupportedException{
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the credit card holder name:");
-        String cname = sc.next();
-        System.out.println("Enter the credit card number:");
-        Integer cno = sc.nextInt();
-        System.out.println("Enter the expiration date:");
-        Integer expdate = sc.nextInt();
-        credit c1=new credit(cname,cno,expdate);
-        credit c2= c1.cr();
-        c2.eq();
+        System.out.println("enter the x coordinate:");
+        Integer x = sc.nextInt();
+        System.out.println("enter the y coordinate:");
+        Integer y = sc.nextInt();
+        Point p1=new Point(x,y);
+        Point p2=(Point)p1.clone();
+        p2.eq();
     }
 }
